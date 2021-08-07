5c0189e9364724abdb9285095809f70a4ca040e0
Filipe <filipediascontato@gmail.com>
2021-08-07 19:28:59 -0300

    Implements the GamesRepository consults

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,26 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
+    return await this.repository
+      .createQueryBuilder("games")
+      .where("games.title ILIKE :param", { param: `%${param}%` })
+      .getMany();
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT COUNT(*) FROM games"); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("games")
+      .innerJoinAndSelect("games.users", "users")
+      .where("games.id = :id", { id })
+      .getOneOrFail()
+      .then(({users}) => {
+        return users;
+      });
       // Complete usando query builder
   }
 }
